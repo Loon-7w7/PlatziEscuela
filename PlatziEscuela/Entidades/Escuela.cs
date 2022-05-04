0934035289a10e96b3bed1788c482c31b58a4244
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-04 16:42:04 -0500

    Agregar archivos de proyecto.

---
+++ PlatziEscuela/Entidades/Escuela.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatziEscuela.Entidades
+{
+    class Escuela
+    {
+        public string Nombre { get; set; }
+        public int AñorDeCreacion { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        private int myvar;
+
+        public Escuela(string Nombre, int AñorDeCreacion) => (this.Nombre, this.AñorDeCreacion) = (Nombre, AñorDeCreacion);
+    }
+}
---
+++ PlatziEscuela/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PlatziEscuela.Entidades;
+
+namespace PlatziEscuela
+{
+    class Program
+    {
+        private const string NombreEscuela = "Platzi Academy";
+        private const string CiudadEscuela = "Bigota";
+        private const string PaisEscuela = "Colimbia";
+
+        static void Main(string[] args)
+        {
+            var ObjEscuela = new Escuela(NombreEscuela, 2012);
+            ObjEscuela.Pais = PaisEscuela;
+            ObjEscuela.Ciudad = CiudadEscuela;
+            Console.WriteLine(ObjEscuela.Nombre);
+        }
+    }
+}
