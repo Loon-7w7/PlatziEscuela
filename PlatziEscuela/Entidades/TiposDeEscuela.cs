23565c3943c27e05f44b5d0c044e4cba03c9a2dc
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-04 16:53:22 -0500

    Cambiops video 12

---
+++ PlatziEscuela/Entidades/TiposDeEscuela.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatziEscuela.Entidades
+{
+    public enum TiposDeEscuela
+    {
+        Primaria,
+        secuandaria,
+        prescolar
+    }
+}
