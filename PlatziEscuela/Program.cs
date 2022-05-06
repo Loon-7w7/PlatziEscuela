fd35658beeb9b7adda66a3eaf9d0fd08b4d54934
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-06 09:31:45 -0500

    Cambios del video 16 Crea un arreglo de objetos

--- PlatziEscuela/Program.cs
+++ PlatziEscuela/Program.cs
@@ -14,15 +14,37 @@ namespace PlatziEscuela
             var ObjEscuela = new Escuela(NombreEscuela, 2012, TiposDeEscuela.Primaria,
                 Pais:"Colombia", Ciudad:"Bobota"
                 );
-            var Cursos = new Curso() { 
-            NombreCurso ="101;"
+
+            var ArregloCursos = new Curso[3];
+
+            ArregloCursos[0] = new Curso()
+            {
+                NombreCurso = "101"
+            };
+
+            var curso2 = new Curso()
+            {
+                NombreCurso = "102"
+            };
+
+            ArregloCursos[1] = curso2;
+            ArregloCursos[2] = new Curso {
+            NombreCurso = "103"
             };
 
             ObjEscuela.Pais = PaisEscuela;
             ObjEscuela.Ciudad = CiudadEscuela;
             Console.WriteLine(ObjEscuela);
             Console.WriteLine("================");
-            Console.WriteLine(Cursos.NombreCurso+","+Cursos.IdentidicadorUnico);
+            ImprimirCursos(ArregloCursos);
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            foreach(var CursosN in arregloCursos)
+            {
+                Console.WriteLine($"Nombre: {CursosN.NombreCurso}, Id: {CursosN.IdentidicadorUnico}");
+            }
         }
     }
 }
