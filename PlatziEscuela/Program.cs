e90c61da859c17e18805873bd9ebf19fcc1899d4
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-06 10:51:54 -0500

    Cambiosdel video 23 Implementando colecciones

--- PlatziEscuela/Program.cs
+++ PlatziEscuela/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PlatziEscuela.Entidades;
 using static System.Console;
 
@@ -9,45 +10,48 @@ namespace PlatziEscuela
         private const string NombreEscuela = "Platzi Academy";
         private const string CiudadEscuela = "Bigota";
         private const string PaisEscuela = "Colimbia";
+        private const string Separador = "================";
+        private const string TextoCursosEscuela = "Cursos de la escuela";
 
         static void Main(string[] args)
         {
             var ObjEscuela = new Escuela(NombreEscuela, 2012, TiposDeEscuela.Primaria,
-                Pais:"Colombia", Ciudad:"Bobota"
+                Pais: PaisEscuela, Ciudad: CiudadEscuela
                 );
 
-           
 
-            ObjEscuela.CursosArreglo = new Curso[] {
-            new Curso(){ NombreCurso = "101"},
-            new Curso(){ NombreCurso = "102"},
-            new Curso { NombreCurso = "103"}
+            ObjEscuela.CursosLista = new List<Curso>() {
+            new Curso(){ NombreCurso = "101", TipoDeJornada = TiposJornada.Mañana },
+            new Curso(){ NombreCurso = "102", TipoDeJornada = TiposJornada.Mañana },
+            new Curso(){ NombreCurso = "103", TipoDeJornada = TiposJornada.Mañana }
             };
+            ObjEscuela.CursosLista.Add(new Curso(){NombreCurso ="201", TipoDeJornada = TiposJornada.Tarde });
+            ObjEscuela.CursosLista.Add(new Curso(){ NombreCurso = "202", TipoDeJornada = TiposJornada.Tarde });
 
-            ImprimirCursosEscuela(ObjEscuela);
 
             ObjEscuela.Pais = PaisEscuela;
             ObjEscuela.Ciudad = CiudadEscuela;
             WriteLine(ObjEscuela);
-            WriteLine("================");
-            ImprimirCursos(ObjEscuela.CursosArreglo);
+            WriteLine(Separador);
+            ImprimirCursos(ObjEscuela.CursosLista);
+            ImprimirCursosEscuela(ObjEscuela);
         }
 
         private static void ImprimirCursosEscuela(Escuela objEscuela)
         {
-            WriteLine("==========");
-            WriteLine("Cursos de la escuela");
-            WriteLine("==========");
-            if (objEscuela?.CursosArreglo != null) {
+            WriteLine(Separador);
+            WriteLine(TextoCursosEscuela);
+            WriteLine(Separador);
+            if (objEscuela?.CursosLista != null) {
 
-                ImprimirCursos(objEscuela.CursosArreglo);
+                ImprimirCursos(objEscuela.CursosLista);
             }
 
            
         }
-        private static void ImprimirCursos(Curso[] arregloCursos)
+        private static void ImprimirCursos(List<Curso> ListaCursos)
         {
-            foreach(var CursosN in arregloCursos)
+            foreach(var CursosN in ListaCursos)
             {
                 WriteLine($"Nombre: {CursosN.NombreCurso}, Id: {CursosN.IdentidicadorUnico}");
             }
