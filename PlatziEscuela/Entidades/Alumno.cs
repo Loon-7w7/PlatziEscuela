f6e3db5e4b3bc424b29b7cf3f039eb992dd04b00
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-06 12:39:46 -0500

    cabios del video 27 Creación de entidades

---
+++ PlatziEscuela/Entidades/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatziEscuela.Entidades
+{
+    public class Alumno
+    {
+        public string IdentidicadorUnico { get; private set; }
+        public string NombreAlumno { get; set; }
+        public Alumno() => IdentidicadorUnico = Guid.NewGuid().ToString();
+    }
+}
---
+++ PlatziEscuela/Entidades/Asignatura.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatziEscuela.Entidades
+{
+    public class Asignatura
+    {
+        public string IdentidicadorUnico { get; private set; }
+        public string NombreAsignatura { get; set; }
+        public Asignatura() => IdentidicadorUnico = Guid.NewGuid().ToString();
+    }
+}
