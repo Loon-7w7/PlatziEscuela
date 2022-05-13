72bacd69a8969de71c09b2e163399b580c6f1eba
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-13 12:31:11 -0500

    Inicio Etapa 10

---
+++ PlatziEscuela/constanttexts/SConstanTextMostrarmenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatziEscuela.constanttexts
+{
+    public class SConstanTextMostrarmenu
+    {
+        public List<string> ShowSelector { get; private set; }
+
+
+        public SConstanTextMostrarmenu() 
+        {
+            InitializeShowSelector();
+        }
+
+
+        private void InitializeShowSelector() 
+        {
+            ShowSelector = new List<string> 
+            {
+                "Reporteador de Escuela",
+                "1. Mostrar lista de evaluaciones",
+                "2. Mostrar lista de evaluaciónes por asignatura",
+                "3. Mostrar lista de asignaturas",
+                "4. Mostrar lista de promedios por asignatura",
+                "5. Mostrar top de promedios por asignatura",
+                "6. Mostrar top de promedios de una asignatura",
+                "7. Finalizar Programa",
+                "\nIngrese el número de la opción del reporteador",
+            };
+        }
+    }
+}
